0df0f0721d341dde094a2407ad80f60f61a49f55
Sorrelljs <[sjsohota@gmail.com]>
2019-11-06 20:28:23 -0800

    making the ball movement

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -2,12 +2,31 @@ import { SVG_NS } from "../settings";
 
 
 export default class Ball {
-    constructor(radius, x, y) {
+    constructor(radius, boardWidth, boardHeight) {
 
         this.radius = radius;
-        this.x = x;
-        this.y = y;
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        this.direction = 1;
+        this.reset();
+    }
+
+        ballMove() {
+            this.x = this.x + this.vx;
+            this.y = this.y + this.vy;
+        }
     
+
+
+    reset(){
+    
+        this.x = this.boardWidth/2;
+        this.y = this.boardHeight/2;
+        this.vy = 1;
+        while (this.y === 0) {
+            this.vy = Math.floor(Math.random() * 10) - 5;
+        }
+        this.vx = this.direction * (6 - Math.abs(this.vy));
     }
   
 
@@ -19,6 +38,8 @@ export default class Ball {
       ball.setAttributeNS(null, "cy", this.y);
       ball.setAttributeNS(null, "fill", "white");
       svg.appendChild(ball);
-}
+      this.ballMove();
+      this.wallCollision();
+    }
 
 }
